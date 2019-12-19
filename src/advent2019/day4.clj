c73f2facc0d1595e23d7e08262833c689627eee2
jasonchadwick <jasonchadwick@users.noreply.github.com>
2019-12-19 14:16:45 -0500

    Fix day4

--- src/advent2019/day4.clj
+++ src/advent2019/day4.clj
@@ -32,9 +32,8 @@
   (let [p (fn [x] (and (not (== 0 (digit x 5)))
                        (== 0 (digit x 6))
                        (<= min x) (<= x max)
-                       (reduce (fn [a b] (or a b)) false
+                       (some identity
                                (apply-adjacent (partial only-equal x) (range 6) nil))
-                       ;(reduce (fn [a b] (and a b)) true
-                        ;       (apply-adjacent (fn [a b] (<= (digit x a) (digit x b))) (range 6) nil))
-                       ))]
-    `(~(lazy-filter p (range min (inc max))))))
+                       (every? identity
+                               (apply-adjacent (fn [a b] (>= (digit x a) (digit x b))) (range 6) nil))))]
+    (count (lazy-filter p (range min (inc max))))))
