f6c4032c0d0c4c4cec831a854ddb0f50f727c786
jasonchadwick <jasonchadwick@users.noreply.github.com>
2019-12-22 14:20:19 -0500

    typo

--- src/advent2019/day1.clj
+++ src/advent2019/day1.clj
@@ -9,7 +9,7 @@
 ;part II
 (defn add-fuels [filename]
   (with-open [rdr (clojure.java.io/reader filename)]
-    (reduce + (map #(fuel (Integer/parseInt str))
+    (reduce + (map #(fuel (Integer/parseInt %))
                (line-seq rdr)))))
 
 ;answer for part 2
