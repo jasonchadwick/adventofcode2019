9d0364dbd4229ae46cea69fc91955da09d050e86
jasonchadwick <jasonchadwick@users.noreply.github.com>
2020-01-07 17:56:22 -0500

    optimized day 10, much faster now

--- src/advent2019/day10.clj
+++ src/advent2019/day10.clj
@@ -2,53 +2,6 @@
   (:require [advent2019.util :as util])
   (:require [clojure.java.io]))
 
-(defn is-blocked-by [board pt deltax deltay [r c]]
-  (let [dy (- r (first pt))
-        dx (- c (second pt))]
-    (cond (or (= 0 dy dx)
-              (not= \# (nth (nth board r) c))
-              (util/only-or(= 0 dx) (= 0 deltax))
-              (and (= 0 dx deltax) (util/diff-signs dy deltay)))
-          false
-          (and (or (= 0 dx deltax)
-                       (= (/ dy dx) (/ deltay deltax)))
-               (or (> (Math/abs deltax) (Math/abs dx))
-                   (> (Math/abs deltay) (Math/abs dy))))
-          true
-          :else false)))
-
-(defn is-visible-from [board row col pt]
-  (let [deltay (- row (first pt))
-        deltax (- col (second pt))]
-    (and (= \# (nth (nth board row) col))
-         (not-any? (partial is-blocked-by board pt deltax deltay)
-                   (map vec (util/prod (range (min (first pt) row)
-                                              (inc (max (first pt) row)))
-                                       (range (min (second pt) col)
-                                              (inc (max (second pt) col)))))))))
-
-(defn num-visible [board pt]
-  (apply +
-         (for [row (range (count board))
-               col (range (count (first board)))]
-           (if (and (not (and (= row (first pt)) (= col (second pt))))
-                    (is-visible-from board row col pt))
-             1 0))))
-
-(defn most-visible [fname]
-  (with-open [rdr (clojure.java.io/reader fname)]
-    (let [board (line-seq rdr)]
-      (reduce #(if (>= (first %1) (first %2)) %1 %2)
-              (for [row (range (count board))
-                    col (range (count (first board)))]
-                `(~(if (= \# (nth (nth board row) col))
-                     (num-visible board `(~row ~col))
-                     0)
-                  ~row
-                  ~col))))))
-
-; part 2
-
 (defn get-rocks [fname]
   (with-open [rdr (clojure.java.io/reader fname)]
     (let [board (line-seq rdr)]
@@ -59,7 +12,7 @@
 ; returns list where keys are slopes, and values are lists of points 
 ; with that slope (slope WRT pt)
 (defn get-slope-map [fname pt]
-  (into 
+  (into
    (sorted-map)
    (let [grid (get-rocks fname)]
      (util/mk-assoc-list
@@ -70,7 +23,21 @@
       grid
       (fn [x] (sort-by #(util/dist (first %) (second %)) x))))))
 
-; retrieves next available item from a non-empty sublist.
+(defn most-visible [fname]
+  (with-open [rdr (clojure.java.io/reader fname)]
+    (let [board (line-seq rdr)]
+      (reduce #(if (>= (first %1) (first %2)) %1 %2)
+              (for [row (range (count board))
+                    col (range (count (first board)))]
+                `(~(if (= \# (nth (nth board row) col))
+                     (count (get-slope-map fname `(~row ~col)))
+                     0)
+                  ~row
+                  ~col))))))
+
+; part 2
+
+; retrieves next non-nil item from a list of lists
 ; returns found item consed with its index and a new coll without the item.
 ; loops infinitely if coll is all nil's.
 (defn get-next [n coll]
---
+++ src/advent2019/legacy-code.clj
@@ -0,0 +1,55 @@
+(ns advent2019.legacy
+  (:require [advent2019.util :as util]))
+
+;code that I've thrown out but don't want to delete because I spent a lot of time on it
+
+;;;;;;;;;;;;;;;;;;;;
+; DAY 10
+;;;;;;;;;;;;;;;;;;;;
+
+(defn is-blocked-by [board pt deltax deltay [r c]]
+  (let [dy (- r (first pt))
+        dx (- c (second pt))]
+    (cond (or (= 0 dy dx)
+              (not= \# (nth (nth board r) c))
+              (util/only-or (= 0 dx) (= 0 deltax))
+              (and (= 0 dx deltax) (util/diff-signs dy deltay)))
+          false
+          (and (or (= 0 dx deltax)
+                       (= (/ dy dx) (/ deltay deltax)))
+               (or (> (Math/abs deltax) (Math/abs dx))
+                   (> (Math/abs deltay) (Math/abs dy))))
+          true
+          :else false)))
+
+(defn is-visible-from [board row col pt]
+  (let [deltay (- row (first pt))
+        deltax (- col (second pt))]
+    (not-any? (partial is-blocked-by board pt deltax deltay)
+              (map vec (util/prod (range (min (first pt) row)
+                                         (inc (max (first pt) row)))
+                                  (range (min (second pt) col)
+                                         (inc (max (second pt) col))))))))
+
+(defn num-visible [board pt]
+  (apply +
+         (for [row (range (count board))
+               col (range (count (first board)))]
+           (if (and (= \# (nth (nth board row) col))
+                    (not (and (= row (first pt)) (= col (second pt))))
+                    (is-visible-from board row col pt))
+             1 0))))
+
+(defn most-visible [fname]
+  (with-open [rdr (clojure.java.io/reader fname)]
+    (let [board (line-seq rdr)]
+      (reduce #(if (>= (first %1) (first %2)) %1 %2)
+              (for [row (range (count board))
+                    col (range (count (first board)))]
+                `(~(if (= \# (nth (nth board row) col))
+                     (num-visible board `(~row ~col))
+                     0)
+                  ~row
+                  ~col))))))
+
+
